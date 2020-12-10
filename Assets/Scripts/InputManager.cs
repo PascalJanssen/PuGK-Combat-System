573397b73d1d3c056b1e072b5c62c46e6bc6298b
PascalJanssen <47032756+PascalJanssen@users.noreply.github.com>
2020-12-10 17:39:03 +0100

    Added Camera and Movement

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private MovementInput Player;
+    private float lastWalkTime;
+    public float walkSpeed = 5;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(lastWalkTime > Time.fixedTime - 0.5f)
+            {
+                Player.Velocity = 3 + walkSpeed * ((Time.fixedTime - lastWalkTime) / 0.5f);
+                Player.anim.SetFloat("Blend", 1 + walkSpeed * ((Time.fixedTime - lastWalkTime) / 0.5f));
+            }
+            else
+            {
+                Player.Velocity = walkSpeed;
+                Player.anim.SetFloat("Blend", 1 + walkSpeed);
+            }
+
+        }
+        else
+        {
+            Player.Velocity = 3;
+            lastWalkTime = Time.fixedTime;
+        }
+        //MovementInputs();
+    }
+
+    private void MovementInputs()
+    {
+        Vector3 direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+
+        //Player.Move(direction);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed = 1;
+
+    [SerializeField]
+    private Animator animator;
+
+    public void Move(Vector3 direction)
+    {
+        transform.position += direction * Time.deltaTime * speed;
+        animator.SetFloat("Move", direction.z);
+    }
+}
